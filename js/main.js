ec07d174f01b9f41f6fde566f28dd1d96b218ebb
Vladislav Surin <vlad_surin@mail.ru>
2023-02-11 00:11:35 +0300

    Added functions for opening and closing a modal window

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const modalAdd = document.querySelector('#modal-add'),
+          modalChange = document.querySelector('#modal-change'),
+          modalDelete = document.querySelector('#modal-delete');
+
+    function openModal(modal) {
+        modal.classList.add('is-open-modal');
+        modal.querySelector('.modal__content').classList.add('modal-open');
+    }
+
+    function closeModal(modal) {
+        modal.classList.remove('is-open-modal');
+        modal.querySelector('.modal__content').classList.remove('modal-open');
+    }
+
+    document.querySelector('.add-client-btn').addEventListener('click', () => {
+        openModal(modalAdd);
+    });
+
+    modalAdd.addEventListener('click', e => {
+        const event = e.target;
+        if (event === modalAdd ||
+            event.closest('.close-btn') ||
+            event.closest('.cansel-btn')) {
+            closeModal(modalAdd);
+        }
+    });
+});
