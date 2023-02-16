bb46d8ab16ee362cb05a96f84d77d2ded0a0e228
Vladislav Surin <vlad_surin@mail.ru>
2023-02-16 21:40:49 +0300

    Added function to create a contact input

--- js/main.js
+++ js/main.js
@@ -30,6 +30,52 @@ window.addEventListener('DOMContentLoaded', () => {
              .classList.remove('modal-open', 'animate-open');
     }
 
+    function createContactInput (parent, showDeleteBtn = false) {
+        const contact = createElement('div', parent, '', ['contact']);
+        if (showDeleteBtn) {
+            contact.classList.add('show-delete-btn');
+        }
+        contact.innerHTML =
+            `
+                <select class="contact__select" name="" id="">
+                    <option value="">Телефон</option>
+                    <option value="">Доп. телефон</option>
+                    <option value="">Email</option>
+                    <option value="">Vk</option>
+                    <option value="">Facebook</option>
+                </select>
+                <input
+                    class="contact__input"
+                    type="text"
+                    placeholder="Введите данные контакта"
+                >
+                <button class="btn-reset contact__delete-btn" type="button">
+                    <svg
+                        class="contact__svg-delete"
+                        width="12" height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M6 0C2.682 0 0 2.682 0 6C0 9.318 2.682 12 6
+                            12C9.318 12 12 9.318 12 6C12 2.682 9.318 0 6 0ZM6
+                            10.8C3.354 10.8 1.2 8.646 1.2 6C1.2 3.354 3.354
+                            1.2 6 1.2C8.646 1.2 10.8 3.354 10.8 6C10.8 8.646
+                            8.646 10.8 6 10.8ZM8.154 3L6 5.154L3.846 3L3
+                            3.846L5.154 6L3 8.154L3.846 9L6 6.846L8.154 9L9
+                            8.154L6.846 6L9 3.846L8.154 3Z"
+                        />
+                    </svg>                                    
+                </button>
+            `;
+        new Choices(contact.querySelector('.contact__select'), {
+            itemSelectText: '',
+            searchEnabled: false,
+            position: 'bottom',
+            shouldSort: false,
+        });
+    }
+
     async function postData(url, data) {
         const result = await fetch(url, {
             method: 'POST',
@@ -44,6 +90,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalAdd = document.querySelector('#modal-add'),
           formAdd = modalAdd.querySelector('.modal-form'),
           inputsFormAdd = formAdd.querySelectorAll('.modal-form__input'),
+          contactsContainerForm = formAdd.querySelector('.modal-form__contacts-container'),
           errorsFormAdd = formAdd.querySelector('.modal-form__errors-box'),
           acceptAddBtn = formAdd.querySelector('.accept-btn'),
           modalChange = document.querySelector('#modal-change'),
@@ -112,12 +159,4 @@ window.addEventListener('DOMContentLoaded', () => {
                 acceptAddBtn.disabled = false;
             });
     });
-
-    // const select = document.querySelector('.contact__select');
-    // new Choices(select, {
-    //     itemSelectText: '',
-    //     searchEnabled: false,
-    //     position: 'bottom',
-    //     shouldSort: false,
-    // });
 });
