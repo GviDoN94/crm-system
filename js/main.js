c4b38785601744db6e9e49f8769031b176f69c34
Vladislav Surin <vlad_surin@mail.ru>
2023-02-22 02:01:57 +0300

    Added functions for client and table rendering

--- js/main.js
+++ js/main.js
@@ -56,43 +56,42 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function createContactInput (parent) {
         const contact = createElement('div', parent, '', ['contact']);
-        contact.innerHTML =
-            `
-                <select class="contact__select">
-                    <option value="Телефон">Телефон</option>
-                    <option value="Email">Email</option>
-                    <option value="Vk">Vk</option>
-                    <option value="Facebook">Facebook</option>
-                    <option value="Другое">Другое</option>
-                </select>
-                <input
-                    class="contact__input"
-                    type="text"
-                    placeholder="Введите данные контакта"
-                >
-                <button
-                    class="btn-reset contact__delete-btn"
-                    type="button"
-                    aria-label="Удалить контакт"
-                >
-                    <svg
-                        class="contact__svg-delete"
-                        width="12" height="12"
-                        viewBox="0 0 12 12"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M6 0C2.682 0 0 2.682 0 6C0 9.318 2.682 12 6
-                            12C9.318 12 12 9.318 12 6C12 2.682 9.318 0 6 0ZM6
-                            10.8C3.354 10.8 1.2 8.646 1.2 6C1.2 3.354 3.354
-                            1.2 6 1.2C8.646 1.2 10.8 3.354 10.8 6C10.8 8.646
-                            8.646 10.8 6 10.8ZM8.154 3L6 5.154L3.846 3L3
-                            3.846L5.154 6L3 8.154L3.846 9L6 6.846L8.154 9L9
-                            8.154L6.846 6L9 3.846L8.154 3Z"
-                        />
-                    </svg>                                    
-                </button>
-            `;
+        contact.innerHTML =`
+            <select class="contact__select">
+                <option value="Телефон">Телефон</option>
+                <option value="Email">Email</option>
+                <option value="Vk">Vk</option>
+                <option value="Facebook">Facebook</option>
+                <option value="Другое">Другое</option>
+            </select>
+            <input
+                class="contact__input"
+                type="text"
+                placeholder="Введите данные контакта"
+            >
+            <button
+                class="btn-reset contact__delete-btn"
+                type="button"
+                aria-label="Удалить контакт"
+            >
+                <svg
+                    class="contact__svg-delete"
+                    width="12" height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M6 0C2.682 0 0 2.682 0 6C0 9.318 2.682 12 6
+                        12C9.318 12 12 9.318 12 6C12 2.682 9.318 0 6 0ZM6
+                        10.8C3.354 10.8 1.2 8.646 1.2 6C1.2 3.354 3.354
+                        1.2 6 1.2C8.646 1.2 10.8 3.354 10.8 6C10.8 8.646
+                        8.646 10.8 6 10.8ZM8.154 3L6 5.154L3.846 3L3
+                        3.846L5.154 6L3 8.154L3.846 9L6 6.846L8.154 9L9
+                        8.154L6.846 6L9 3.846L8.154 3Z"
+                    />
+                </svg>                                    
+            </button>
+        `;
         new Choices(contact.querySelector('.contact__select'), {
             itemSelectText: '',
             searchEnabled: false,
@@ -128,6 +127,36 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    function renderClient (obj, parent) {
+        const client = createElement('tr', parent, '', ['client']);
+        client.innerHTML =`
+            <td class="table__client-cell client__id">${obj.id}</td>
+            <td class="table__client-cell client__name">${obj.name}</td>
+            <td class="table__client-cell client__creation">
+                <span class="client__creation-date">21.02.2021</span>
+                <span class="client__creation-time">12:41</span>
+            </td>
+            <td class="table__client-cell client__change">
+                <span class="client__change-date">21.02.2021</span>
+                <span class="client__change-time">14:50</span>
+            </td>
+            <td class="table__client-cell client__contacts">
+                <div class="client__contacts-wrap"></div>
+            </td>
+            <td class="table__client-cell client__actions">
+                <button class="client__change-btn btn-reset">Изменить</button>
+                <button class="client__delete-btn btn-reset">Удалить</button>
+            </td>
+        `;
+    }
+
+    function renderTable(arr, parent) {
+        const copyArr = [...arr];
+        parent.innerHTML = '';
+
+        copyArr.forEach(client => renderClient(client, parent));
+    }
+
     async function getData(url) {
         const result = await fetch(url);
         return result.json();
@@ -144,7 +173,8 @@ window.addEventListener('DOMContentLoaded', () => {
         return await result.json();
     }
 
-    const modalAdd = document.querySelector('#modal-add'),
+    const tBody = document.querySelector('.table__body'),
+          modalAdd = document.querySelector('#modal-add'),
           formAdd = modalAdd.querySelector('.modal-form'),
           inputsFormAdd = formAdd.querySelectorAll('.modal-form__input'),
           addContactsFormAdd = formAdd.querySelector(
@@ -267,6 +297,6 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     getData('http://localhost:3500/api/clients')
-        .then(data => console.log(data))
+        .then(data => renderTable(data, tBody))
         .catch(() => console.log('Что-то пошло не так'));
 });
